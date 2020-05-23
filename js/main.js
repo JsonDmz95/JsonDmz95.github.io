c820999dfb98a360e07eca5ca84df10b5913f8d2
Jeeson Dominguez <dominguez_js@outlook.com>
2020-05-22 21:55:16 -0600

    fixing fluid scrol

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-$(window).on('load', function() {
-
-    $(this).impulse();
+$("html").easeScroll({
+    frameRate: 60,
+    animationTime: 1500,
+    stepSize: 200,
+    pulseAlgorithm: 1,
+    pulseScale: 8,
+    pulseNormalize: 1,
+    accelerationDelta: 20,
+    accelerationMax: 1,
+    keyboardSupport: true,
+    arrowScroll: 50,
+    touchpadSupport: true,
+    fixedBackground: true
   });
