c77f74cfd6f1a5d9b49c8527572c11408b5b16fd
Jeeson Domiguez <dominguez_js@outlook.com>
2020-05-24 00:57:37 -0600

    resume ready

--- js/tools.js
+++ js/tools.js
@@ -1,3 +1,4 @@
+// show elemnts with hover 
 $(window).on("load", function(){
     $('.transparent-link').hover(function(){
         $($(this).data("target")).addClass("in");
@@ -5,3 +6,15 @@ $(window).on("load", function(){
         $($(this).data("target")).removeClass("in");
     });
 });
+// END OF show elemnts with hover
+
+// Local Scroll 
+$('.nav-link').click(function(e) {
+    var jump = $(this).attr('href');
+    var position = $(jump).offset();
+    $('body, html').stop().animate({
+        scrollTop: position.top
+    }, 1400);
+    e.preventDefault();
+});
+// END OF Local Scroll 
