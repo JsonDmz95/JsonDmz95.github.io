e6d731f07f226ef3c77653bc157b9ed8d6872933
Jeeson Domiguez <dominguez_js@outlook.com>
2020-06-13 23:50:21 -0600

    Loading functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-$(window).on('load', function() {
+$(window).on('load', function () {
 
   $(this).impulse();
+  $("body").css("overflow-y", "scroll");
+  $(".preload").addClass("hide");
 
+  setTimeout(function () {
+    $(".preload").css("display", "none");
+  }, 1000);
   checkCookie();
 });
